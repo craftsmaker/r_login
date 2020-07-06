d0d47903fbec9a20c7cd19ef4bf8edf016a92841
Victor Guedes <victorguedes68@hotmail.com>
2020-07-06 19:01:32 -0300

    feat: improve checkbox

--- src/screens/Register/Checkbox/index.js
+++ src/screens/Register/Checkbox/index.js
@@ -4,14 +4,32 @@ import LinkedButton from "../../../components/LinkedButton";
 import styles from "./styles.module.css";
 
 export default () => {
-    const [checked,setChecked] = React.useState(false);
-    return(
-        <div style={{display: "flex",alignItems: "center"}}>
-            <div className={styles.checkboxSelection} onClick={() => setChecked(checked => !checked)}>
-                <input className={styles.checkbox} checked={checked} onChange={() => {}} type="checkbox" id="termsOfUse" style={{marginRight: "10px"}}/>
-                <span className={styles.checkmark}></span>
-            </div>
-            <label htmlFor="termsOfUse" style={{color: colors.fading,fontSize: "1em"}}>I agree to the terms statements in <LinkedButton style={{display: "inline"}} to="/TermsOfServices">Terms of services</LinkedButton></label>
-        </div>
-    )
-}
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div className={styles.checkboxSelection}>
+        <input
+          className={styles.checkbox}
+          checked={checked}
+          onChange={() => {}}
+          type="checkbox"
+          id="termsOfUse"
+          style={{ marginRight: "10px" }}
+        />
+        <span
+          className={styles.checkmark}
+          onClick={() => setChecked((checked) => !checked)}
+        ></span>
+      </div>
+      <label
+        htmlFor="termsOfUse"
+        style={{ color: colors.fading, fontSize: "1em" }}
+      >
+        I agree to the terms statements in{" "}
+        <LinkedButton style={{ display: "inline" }} to="/TermsOfServices">
+          Terms of services
+        </LinkedButton>
+      </label>
+    </div>
+  );
+};
